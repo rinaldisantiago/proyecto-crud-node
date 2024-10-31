380f91fa59e382b30bc061f28c6379717dc15d45
rinaldisantiago <santiago.rinaldi@itscordoba.edu.ar>
2024-10-31 10:51:48 -0300

    Rutas agregadas en usuario

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const usuarioController = require("../controllers/usuarioController");
+const authController = require("../controllers/authController");
 const auth = require("../middlewares/authmiddleware");
 const upload = require("../middlewares/uploadMiddleware");
 
 
 router.post("/register", usuarioController.register);
+router.get("/", usuarioController.list);
+router.post("/login", authController.login);
 router.put("/me", auth, upload.single('avatar'), usuarioController.update);
-router.get("/list", usuarioController.list);
 
 module.exports = router;
