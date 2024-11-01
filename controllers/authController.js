ca295eb10f3aaadc6a25ce606814938a704bf7f5
rinaldisantiago <santiago.rinaldi@itscordoba.edu.ar>
2024-11-01 17:12:08 -0300

    Modificaciones en controlador auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,23 +3,19 @@ const jwt = require('jsonwebtoken');
 const db = require('../models'); // Importar los modelos
 const Usuario = db.Usuario; // Acceder al modelo Usuario
 
-
 const login = async(req, res) => {
     const { mail, password } = req.body;    
 
     try {
-        const { mail, password } = req.body;
-        // Se verifica si el mail existe en la base de datos
-        const usuario = await Usuario.findOne({
-            where: mail
-        });
-        if (!usuario){
-            res.status(404).send({ message: "Mail no existente" });
+        //1 - Constatar que existe una cuenta con ese mail
+        const usuario = await Usuario.findOne({ where: { mail } });
+        if (!usuario) {
+            return res.status(404).send({ message: "Usuario no encontrada" });
         }
-        // Se verifica el password
-        const isMatch = await bcrypt.compare(password, Usuario.password);
-        if (!isMatch){
-            res.status(400).send({ message: "Password incorrecto" });
+        //2 - Verificar password
+        const isMatch = await bcrypt.compare(password, usuario.password);
+        if (!isMatch) {
+            return res.status(400).send({ message: "Password incorrecto" });
         }
         //3 - Crear token
         const token = jwt.sign({
