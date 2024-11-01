a425c1620eded458e00fad56290cec7132c07f1e
rinaldisantiago <santiago.rinaldi@itscordoba.edu.ar>
2024-11-01 17:16:51 -0300

    Parte de documentacion añadido en rutas de following

--- routes/followingRoutes.js
+++ routes/followingRoutes.js
@@ -3,6 +3,101 @@ const router = express.Router();
 const followingController = require("../controllers/followingController");
 const auth = require("../middlewares/authmiddleware");
 
-router.post("/", auth, followingController.follow);
+/**
+ * @swagger
+ * /followings/follow/:
+ *   post:
+ *     tags: [Follows]
+ *     summary: Follow a new user
+ *     security:
+ *       - ApiTokenAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id_usuario:
+ *                 type: integer
+ *               id_usuario_seguido:
+ *                 type: integer
+ *     responses:
+ *       201:
+ *         description: You have started following the user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id_usuario:
+ *                   type: integer
+ *                 id_usuario_seguido:
+ *                   type: integer
+ *       400:
+ *         description: You can't follow yourself
+ *       401:
+ *         description: You already follow this user
+ *       500:
+ *         description: Internal server error
+ */
+router.post("/follow", auth, followingController.follow);
 
+
+//!PREGUNTAR
+router.delete("/unfollow", auth, followingController.unfollow);
+
+/**
+ * @swagger
+ * /followings/following:
+ *   get:
+ *     summary: List all followings
+ *     parameters:
+ *       - name: page
+ *         in: query
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         description: Page number for pagination
+ *       - name: limit
+ *         in: query
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         description: Number of users per page
+ *     responses:
+ *       200:
+ *         description: A paginated list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 totalItems:
+ *                   type: integer
+ *                 totalPages:
+ *                   type: integer
+ *                 currentPage:
+ *                   type: integer
+ *                 itemsPerPage:
+ *                   type: integer
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id_usuario_seguido:
+ *                         type: intenger
+ *       400:
+ *         description: Bad request - page or limit must be positive
+ *       500:
+ *         description: Internal server error
+ */
+router.get("/following", auth, followingController.listFollowing);
+router.get("/following", auth, followingController.listFollowing);
+
+router.get("/followers", auth, followingController.listFollowers);
+router.get("/mutual", auth, followingController.listMutualFollowing);
 module.exports = router;
