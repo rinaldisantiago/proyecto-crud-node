338cde538cb0db0009a12a120f15e7c124a917b6
rinaldisantiago <santiago.rinaldi@itscordoba.edu.ar>
2024-11-01 17:17:11 -0300

    Cambios en index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = 3000;
-const personaRouter = require("./routes/personaRoutes");
-const oficinaRouter = require("./routes/oficinaRoutes");
+const swaggerUi = require("swagger-ui-express"); //Para swagger
+const swaggerDocs = require("./swaggerConfig"); // Para swagger
+const path = require("path");
+
 const authRouter = require("./routes/authRoutes");
 const usuarioRouter = require("./routes/usuarioRoutes");
 const followingRouter = require("./routes/followingRoutes");
-const path = require("path");
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); //Ruta swagger
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use("/api/personas", personaRouter);
-app.use("/api/oficinas", oficinaRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/usuarios", usuarioRouter);
 app.use("/api/followings", followingRouter);
