99bb73e691c5e962d73b01d92cdc334e3995ec01
rinaldisantiago <santiago.rinaldi@itscordoba.edu.ar>
2024-11-20 18:23:59 -0300

    Eliminamos controlador y ruta auth

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const swaggerUi = require("swagger-ui-express"); //Para swagger
 const swaggerDocs = require("./swaggerConfig"); // Para swagger
 const path = require("path");
 
-const authRouter = require("./routes/authRoutes");
 const usuarioRouter = require("./routes/usuarioRoutes");
 const followingRouter = require("./routes/followingRoutes");
 const postRouter = require("./routes/postRoutes");
@@ -19,7 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); //Ruta swagger
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use("/api/auth", authRouter);
 app.use("/api/usuarios", usuarioRouter);
 app.use("/api/followings", followingRouter);
 app.use("/api/post", postRouter);
